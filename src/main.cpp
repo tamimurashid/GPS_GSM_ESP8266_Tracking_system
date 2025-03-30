c429184d4a02433d8d05f6ccebb9583a1a7636cc
Tamimu Said Rashid <121192802+tamimurashid@users.noreply.github.com>
2025-03-30 13:41:40 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,43 @@
-#include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
 
-// Define GPS baud rate and RX/TX pins
-static const int RXPin = 4, TXPin = 5;
-static const uint32_t GPSBaud = 9600;
-
-// Create a TinyGPSPlus object
-TinyGPSPlus gps;
-
-// Set up a SoftwareSerial connection for GSM (RX, TX for the GSM module)
-SoftwareSerial gsmSerial(12, 13);  // RX, TX pins for GSM
-
-// Set up a SoftwareSerial connection for GPS (using RXPin and TXPin)
-SoftwareSerial ss(RXPin, TXPin);
-
-// Replace with the phone number you want to send SMS to
-const char* phoneNumber = "+255768857064";  // Example phone number
-
-// Variables for timing
-unsigned long lastUpdateTime = 0;
-const unsigned long updateInterval = 1000; // Update every 1 second
-
-// Function prototype
-void sendSMS(String message);
-
-void setup() {
-  // Start Serial communication for debugging
+//Create software serial object to communicate with SIM800L
+SoftwareSerial mySerial(13, 12); //SIM800L Tx & Rx is connected to Arduino #3 & #2
+void updateSerial();
+void setup()
+{
+  //Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
   Serial.begin(115200);
-  ss.begin(GPSBaud);
-  gsmSerial.begin(9600);  // Set the GSM baud rate
   
-  // Give the GSM module some time to register to the network
+  //Begin serial communication with Arduino and SIM800L
+  mySerial.begin(9600);
+
+  Serial.println("Initializing...");
   delay(1000);
 
-  // Set the GSM module to SMS mode
-  gsmSerial.println("AT+CMGF=1");  // Set to SMS text mode
-  delay(1000);  // Wait for the command to be executed
-
-  Serial.println("GPS and GSM Setup Complete");
+  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+  updateSerial();
+  mySerial.println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
+  updateSerial();
+  mySerial.println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
+  updateSerial();
+  mySerial.println("AT+CREG?"); //Check whether it has registered in the network
+  updateSerial();
 }
 
-void loop() {
-  // Check if the GSM module has received any data
-  if (gsmSerial.available()) {
-    String sms = "";
-    while (gsmSerial.available()) {
-      sms += (char)gsmSerial.read();  // Read incoming SMS data
-    }
-
-    // If the received SMS contains the word "location", send the Google Maps link
-    if (sms.indexOf("location") != -1) {
-      // Retrieve GPS coordinates
-      double latitude = gps.location.lat();
-      double longitude = gps.location.lng();
-      
-      // Create a Google Maps link with the current coordinates
-      String mapLink = "https://www.google.com/maps?q=" + String(latitude, 6) + "," + String(longitude, 6);
-      
-      // Send the Google Maps link via SMS
-      sendSMS(mapLink);
-    }
-  }
-
-  // Update GPS coordinates periodically
-  while (ss.available() > 0) {
-    gps.encode(ss.read());  // Feed the GPS data into the gps object
-  }
-
-  // Check if it's time to update the location
-  if (millis() - lastUpdateTime >= updateInterval) {
-    if (gps.location.isUpdated()) {  // Only print when a new location is received
-      // Print the Latitude and Longitude to the Serial Monitor
-      double latitude = gps.location.lat();
-      double longitude = gps.location.lng();
-      
-      Serial.print("Latitude: ");
-      Serial.print(latitude, 6);  // 6 decimal places
-      Serial.print(" Longitude: ");
-      Serial.println(longitude, 6);  // 6 decimal places
-    }
-
-    // Update the last update time
-    lastUpdateTime = millis();
-  }
+void loop()
+{
+  updateSerial();
 }
 
-// Function to send SMS using the GSM module
-void sendSMS(String message) {
-  gsmSerial.println("AT+CMGS=\"" + String(phoneNumber) + "\"");  // Send SMS to the number
-  delay(1000);
+void updateSerial()
+{
   
-  gsmSerial.print(message);  // Message body
-  delay(100);
-  
-  gsmSerial.write(26);  // Send the "Ctrl+Z" character to indicate end of message
-  delay(5000);  // Wait for the message to be sent
-  Serial.println("SMS Sent: " + message);
+  while (Serial.available()) 
+  {
+    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(mySerial.available()) 
+  {
+    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+  }
 }
